af39d0807e3f44fbbd997feda68dad81b9f6346d
Alexsandro Mendes <rejaneisabella2507@gmail.com>
2023-08-28 14:24:21 -0300

    Add files via upload

--- screenmatch/modelos/Filme.java
+++ screenmatch/modelos/Filme.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.modelos;
 
-    public class Filme extends Titulo {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel{
         private String diretor;
         public String getDiretor() {
             return diretor;
@@ -8,4 +10,9 @@ package br.com.alura.screenmatch.modelos;
         public void setDiretor(String diretor) {
             this.diretor = diretor;
         }
+
+    @Override
+    public int getclassificacao() {
+        return (int) pegaMedia() / 2;
     }
+}
